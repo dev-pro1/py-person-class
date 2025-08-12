5d5649957c656d9c6d1c523b7f278be895de5755
dev-pro1 <dev-pro1@gmail.com>
2025-08-12 12:03:29 +0300

    Solution fix 1

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ class Person:
 
 
 def create_person_list(people: list) -> list:
+    Person.people.clear()
     for person in people:
         name = person.get("name")
         age = person.get("age")
